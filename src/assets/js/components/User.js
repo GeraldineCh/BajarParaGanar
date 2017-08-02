5609656609082f7a9a1ca0604331761229f70c15
Wendy Reyes <wendy.reyessan@gmail.com>
2017-08-02 15:34:13 -0500

    index js

--- src/assets/js/components/User.js
+++ src/assets/js/components/User.js
@@ -1,17 +1,36 @@
 'use strict';
 const User = (update) => {
-	alert("User");
+
 	const container = $('<form></form>');
 	const title = $('<h1>Datos del participante</h1>');
 	const name = $('<div><label for="js_input_name">Nombre</label><input id="js_input_name" type="text"></div>');
 	const lName1 = $('<div><label for="js_input_lname_1">Apellido 1</label><input id="js_input_lname_1" type="text"></div>');
 	const lName2 = $('<div><label for="js_input_lname_2">Apellido 2</label><input id="js_input_lname_2" type="text"></div>');
 	const genero = $('<div><select><option value="" disabled selected>Género</option><option value="1">Femenino</option><option value="2">Masculino</option><option value="3">Otro</option></select></div>');
+	const fNac = $('<input type="text" class="datepicker">');
+	const consSms = $('<div><input type="checkbox" id="test5"/><label for="test5">Recibir sms</label></div>');
+	const dni = $('<div><label for="js_input_dni">DNI</label><input id="js_input_dni" type="text"></div>');
+	const mobile = $('<div><label for="js_input_mobile">Celular</label><input id="js_input_mobile" type="text"></div>');
+	const email = $('<div><label for="js_input_email">Email</label><input id="js_input_email" type="email" class="validate"></div>');
 
 	container.append(title);
 	container.append(name);
 	container.append(lName1);
 	container.append(lName2);
 	container.append(genero);
+	container.append(consSms);
+	container.append(dni);
+	container.append(mobile);
+	container.append(email);
+	/*
+	$('.datepicker').pickadate({
+		selectMonths: true, // Creates a dropdown to control month
+		selectYears: 15, // Creates a dropdown of 15 years to control year,
+		today: 'Today',
+		clear: 'Clear',
+		close: 'Ok',
+		closeOnSelect: false // Close upon selecting a date,
+	});
+	*/
 	return container;
 };
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -3,7 +3,7 @@
 const render = (root) => {
   root.empty();
   const wrapper = $('<div class="wrapper"></div>');
-
+	
 	switch(state.screenView) {
     case null:
       wrapper.append(User(_ => render(root)));
