cfedeb1b09c653dd93006e9ba1720ce7cf6addc7
Wendy Reyes <wendy.reyessan@gmail.com>
2017-08-02 14:41:22 -0500

    first screen

--- src/assets/js/components/User.js
+++ src/assets/js/components/User.js
@@ -1,4 +1,3 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 const User = (update) => {
 	alert("User");
@@ -16,32 +15,3 @@ const User = (update) => {
 	container.append(genero);
 	return container;
 };
-
-'use strict';
-
-const render = (root) => {
-  root.empty();
-  const wrapper = $('<div class="wrapper"></div>');
-
-	switch(state.screenView) {
-    case null:
-      wrapper.append(User(_ => render(root)));
-      break;
-		case "next":
-
-	}
-  root.append(wrapper);
-}
-
-const state = {
-	screenView: null,
-};
-
-$( _ => {
-
-	const root = $('.root');
-	render(root);
-
-});
-
-},{}]},{},[1])
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,26 @@
-  alert();
-  console.log("Inicio");
-  console.log("hjhk");
+'use strict';
+
+const render = (root) => {
+  root.empty();
+  const wrapper = $('<div class="wrapper"></div>');
+
+	switch(state.screenView) {
+    case null:
+      wrapper.append(User(_ => render(root)));
+      break;
+		case "next":
+
+	}
+  root.append(wrapper);
+}
+
+const state = {
+	screenView: null,
+};
+
+$( _ => {
+
+	const root = $('.root');
+	render(root);
+
+});
