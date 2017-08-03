580bd6b1a102e5b7ef93c3faa79d7132b6e69b09
Geraldine Chauca <gchaucae@gmail.com>
2017-08-02 19:37:39 -0500

    Inputs - Radio buttons to select

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -14,52 +14,67 @@ const StateUser = () => {
     const goal1 = $('<label for="goal">Meta de peso: </label>');
     const goal2 = $('<input disabled id="goal" type="text" class="validate">');
     const smoke = $('<div class="input-field col s12"></div>');
-    const smoke1 = $('<label for="smoke">¿Fuma? </label>');
+    const smoke1 = $('<label>¿Fuma?</label>');
     const smoke2 = $('<input name="group1" type="radio" id="smokeSi"/><label for="test1">Sí</label>');
     const smoke3 = $('<input name="group1" type="radio" id="smokeNo"/><label for="test1">No</label>');
+    
     const cantidad = $('<div class="input-field col s12"></div>');
     const cantidad1 = $('<label for="smoke">¿Con qué frecuencia fuma? </label>');
-    const cantidad2 = $('<input name="group2" type="radio" id="smokeDaily"/><label for="test1">Diario</label>');
-    const cantidad3 = $('<input name="group2" type="radio" id="smokeInterdiario"/><label for="test1">Interdiario</label>');
-    const cantidad4 = $('<input name="group2" type="radio" id="smokeSemanal"/><label for="test1">Semanal</label>');
-    const cantidad5 = $('<input name="group2" type="radio" id="smokeQuincenal"/><label for="test1">Quincenal</label>');
-    const cantidad6 = $('<input name="group2" type="radio" id="smokeMensual"/><label for="test1">Mensual</label>');
-    const cantidad7 = $('<input name="group2" type="radio" id="smokeMenorMensual"/><label for="test1">Menos que una vez al mes</label>');
+    const cantidad2 = $('<select id="smoke"></select>');
+    const cantidad3 = $('<option value="" disabled selected>Seleccione</option>');
+    const cantidad4 = $('<option value="1">Diario</option>');
+    const cantidad5 = $('<option value="2">Interdiario</option>');
+    const cantidad6 = $('<option value="3">Semanal</option>');
+    const cantidad7 = $('<option value="4">Quincenal</option>');
+    const cantidad8 = $('<option value="5">Mensual</option>');
+    const cantidad9 = $('<option value="6">Menos que una vez al mes</option>');
+    
     const sleep = $('<div class="input-field col s12"></div>');
-    const sleep1 = $('<label for="sleep">Horas de sueño</label>');
-    const sleep2 = $('<input name="group3" type="radio" id="menorCinco"/><label for="test1">Menos de 5 horas</label>');
-    const sleep3 = $('<input name="group3" type="radio" id="cincoHoras"/><label for="test1">5 horas</label>');
-    const sleep4 = $('<input name="group3" type="radio" id="seisHoras"/><label for="test1">6 horas</label>');
-    const sleep5 = $('<input name="group3" type="radio" id="sieteHoras"/><label for="test1">7 horas</label>');
-    const sleep6 = $('<input name="group3" type="radio" id="ochoHoras"/><label for="test1">8 horas</label>');
-    const sleep7 = $('<input name="group3" type="radio" id="mayorOcho"/><label for="test1">Más de 8 horas</label>');
+    const sleep1 = $('<label for="sleep">Horas de sueño </label>');
+    const sleep2 = $('<select id="sleep"></select>');
+    const sleep3 = $('<option value="" disabled selected>Seleccione</option>'); 
+    const sleep4 = $('<option value="1">Menos de 5 horas</option>');
+    const sleep5 = $('<option value="2">5 horas</option>');
+    const sleep6 = $('<option value="3">6 horas</option>');
+    const sleep7 = $('<option value="4">7 horas</option>');
+    const sleep8 = $('<option value="5">8 horas</option>');
+    const sleep9 = $('<option value="6">Más de 8 horas</option>');
+
     const activity = $('<div class="input-field col s12"></div>');
-    const activity1 = $('<label for="fuma">¿Actividad física al menos 10 miin diarios?</label>');
+    const activity1 = $('<label for="fuma">¿Actividad física al menos 10 min diarios?</label>');
     const activity2 = $('<input name="group4" type="radio" id="activityYes"/><label for="test1">Sí</label>');
     const activity3 = $('<input name="group4" type="radio" id="ativityNo"/><label for="test1">No</label>');
+ 
     const pactivity = $('<div class="input-field col s12"></div>');
-    const pactivity1 = $('<label for="fuma">¿Actividad física al menos 30 miin diarios?</label>');
+    const pactivity1 = $('<label for="fuma">¿Actividad física al menos 30 min diarios?</label>');
     const pactivity2 = $('<input name="group5" type="radio" id="pactivityYes"/><label for="test1">Sí</label>');
     const pactivity3 = $('<input name="group5" type="radio" id="pativityNo"/><label for="test1">No</label>');
+ 
     const drink = $('<div class="input-field col s12"></div>');
-    const drink1 = $('<label for="fuma">¿Toma?</label>');
+    const drink1 = $('<label>¿Toma?</label>');
     const drink2 = $('<input name="group6" type="radio" id="drinkYes"/><label for="test1">Sí</label>');
     const drink3 = $('<input name="group6" type="radio" id="drinkNo"/><label for="test1">No</label>');
+ 
     const dcant = $('<div class="input-field col s12"></div>');
-    const dcant1 = $('<label for="fuma">¿Con qué frecuencia toma? </label>');
-    const dcant2 = $('<input name="group7" type="radio" id="drinkDaily"/><label for="test1">Diario</label>');
-    const dcant3 = $('<input name="group7" type="radio" id="drinkInterdaily"/><label for="test1">Interdiario</label>');
-    const dcant4 = $('<input name="group7" type="radio" id="drinkSemanal"/><label for="test1">Semanal</label>');
-    const dcant5 = $('<input name="group7" type="radio" id="drinkQuincenal"/><label for="test1">Quincenal</label>');
-    const dcant6 = $('<input name="group7" type="radio" id="drinkMensual"/><label for="test1">Mensual</label>');
-    const dcant7 = $('<input name="group7" type="radio" id="drinkMenorMensual"/><label for="test1">Menos que una vez al mes</label>');
+    const dcant1 = $('<label for="dcant">¿Con qué frecuencia toma? </label>');
+    const dcant2 = $('<select id="dcant"></select>');
+    const dcant3 = $('<option value="" disabled selected>Seleccione</option>'); 
+    const dcant4 = $('<option value="1">Diario</option>');
+    const dcant5 = $('<option value="2">Interdiario</option>');
+    const dcant6 = $('<option value="3">Semanal</option>');
+    const dcant7 = $('<option value="4">Quincenal</option>');
+    const dcant8 = $('<option value="5">Mensual</option>');
+    const dcant9 = $('<option value="6">Menos que una vez al mes</option>');
+ 
     const shots = $('<div class="input-field col s12"></div>');
     const shots1 = $('<label for="shots">¿Cuántos vasos?</label>');
-    const shots2 = $('<input name="group8" type="radio" id="oneShot"/><label for="test1">1</label>');
-    const shots3 = $('<input name="group8" type="radio" id="twoShot"/><label for="test1">2</label>');
-    const shots4 = $('<input name="group8" type="radio" id="threeShot"/><label for="test1">3</label>');
-    const shots5 = $('<input name="group8" type="radio" id="fourShot"/><label for="test1">4</label>');
-    const shots6 = $('<input name="group8" type="radio" id="fiveShot"/><label for="test1">Más de 4</label>');
+    const shots2 = $('<select id="shots"></select>');
+    const shots3 = $('<option value="" disabled selected>Seleccione</option>'); 
+    const shots4 = $('<option value="1">1</option>');
+    const shots5 = $('<option value="2">2</option>');
+    const shots6 = $('<option value="3">3</option>');
+    const shots7 = $('<option value="4">4</option>');
+    const shots8 = $('<option value="5">Más de 4</option>');
  
     principal.append(title);
  
@@ -80,19 +95,24 @@ const StateUser = () => {
  
  cantidad.append(cantidad1);
  cantidad.append(cantidad2);
- cantidad.append(cantidad3);
- cantidad.append(cantidad4);
- cantidad.append(cantidad5);
- cantidad.append(cantidad6);
- cantidad.append(cantidad7);
+ cantidad2.append(cantidad3);
+ cantidad2.append(cantidad4);
+ cantidad2.append(cantidad5);
+ cantidad2.append(cantidad6);
+ cantidad2.append(cantidad7);
+ cantidad2.append(cantidad8);
+ cantidad2.append(cantidad9);
  
  sleep.append(sleep1);
  sleep.append(sleep2);
- sleep.append(sleep3);
- sleep.append(sleep4);
- sleep.append(sleep5);
- sleep.append(sleep6);
- sleep.append(sleep7);
+ sleep2.append(sleep3);
+ sleep2.append(sleep4);
+ sleep2.append(sleep5);
+ sleep2.append(sleep6);
+ sleep2.append(sleep7);
+ sleep2.append(sleep8);
+ sleep2.append(sleep9);
+
  
  activity.append(activity1);
  activity.append(activity2);
@@ -108,18 +128,23 @@ const StateUser = () => {
  
  dcant.append(dcant1);
  dcant.append(dcant2);
- dcant.append(dcant3);
- dcant.append(dcant4);
- dcant.append(dcant5);
- dcant.append(dcant6);
- dcant.append(dcant7);
+ dcant2.append(dcant3);
+ dcant2.append(dcant4);
+ dcant2.append(dcant5);
+ dcant2.append(dcant6);
+ dcant2.append(dcant7);
+ dcant2.append(dcant8);
+ dcant2.append(dcant9);
+
  
  shots.append(shots1);
  shots.append(shots2);
- shots.append(shots3);
- shots.append(shots4);
- shots.append(shots5);
- shots.append(shots6);
+ shots2.append(shots3);
+ shots2.append(shots4);
+ shots2.append(shots5);
+ shots2.append(shots6);
+ shots2.append(shots7);
+ shots2.append(shots8);
     
  principal.append(weight);
  principal.append(height);
